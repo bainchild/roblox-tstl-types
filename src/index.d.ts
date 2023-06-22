568fd568c4fa5beeb4c7f525b5506c7e42d88838
bainchild <bainchild@letterbomb.ftp.sh>
2023-06-22 02:18:16 -0500

    Pretend like I actually read the page on publishing modules

---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+export * from "./class_types";
+export * from "./datatypes"
+export declare var game: classes.PVInstance;
+export declare var workspace: classes.PVInstance;
--- src/libraries.ts
+++
@@ -1,7 +0,0 @@
-import * as classes from "./class_types";
-/** @noSelf **/
-declare namespace Instance {
-    function new_(x: string): classes.Instance;
-    function new_(x: string, p: classes.Instance): classes.Instance;
-    export {new_ as new}
-}
