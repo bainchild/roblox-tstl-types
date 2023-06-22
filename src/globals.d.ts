8c393db3a6e91a6d012f1da3f641b6db87285435
bainchild <bainchild@letterbomb.ftp.sh>
2023-06-22 01:43:42 -0500

    Remove artifacts from previous build.

--- src/globals.d.ts
+++
@@ -1,7 +0,0 @@
-import * as classes from "./class_types";
-
-/** @noSelf **/
-export declare function print(...text: any[]): void;
-
-export let game: classes.PVInstance;
-export let workspace: classes.PVInstance;
